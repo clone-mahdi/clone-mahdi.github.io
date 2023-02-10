437df923884863f6e3941f7473bf8364c5088614
mahdi41.202@gmail.com <mahdi41.202@gmail.com>
2023-02-10 14:20:19 +0330

    hh

--- script/404.js
+++ script/404.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", ()=> {
    const body = document.body;
+   const h1 = document.querySelector("h1");
    setInterval(() => {
       var e = document.createElement("span");
       var dur = ((Math.random()*2000) + 3000);
@@ -14,5 +15,20 @@ document.addEventListener("DOMContentLoaded", ()=> {
       setTimeout(() => {
          e.remove();
       }, delay + 5000);
-   }, 200);
+   }, 500);
+   h1.onclick = () => {
+      var e = document.createElement("span");
+      var dur = ((Math.random() * 2000) + 3000);
+      var delay = 0;
+      body.appendChild(e);
+      e.classList.add("e");
+      e.innerHTML = "404";
+      e.style.left = Math.random() * Number(window.innerWidth) + "px";
+      e.style.fontSize = Math.random() * 20 + 10 + "px";
+      e.style.color = "hsla(" + Math.floor(Math.random() * 90 + 180) + ", 100%, 50%," + Math.floor(Math.random() * 50 + 50) + "%)";
+      e.style.animation = "rain " + dur + "ms both linear " + delay + "ms";
+      setTimeout(() => {
+         e.remove();
+      }, delay + 5000);
+   }
 });
